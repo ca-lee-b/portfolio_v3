bb8507ca19138b7b7dab4e35db5161099cd2af15
dots225 <kaluhblee@gmail.com>
2022-06-08 18:37:33 -0500

    Fix image not loading

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
 				glow: '0px 10px 48px -4px rgba(185,28,28,0.75)'
 			},
 			backgroundImage: {
-				hackftw: 'url(src/assets/hackftw.png)',
+				hackftw: '/hackftw.png',
 				placeholder2: "url('https://via.placeholder.com/1920x1080')"
 			}
 		}
